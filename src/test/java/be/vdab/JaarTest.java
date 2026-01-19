7b9e007b3ed0912e27dbacff2ddeff4ab5365a2b
Eric Tatarli <eric.ed.tatarlihotmail.com>
2026-01-19 16:06:32 +0100

    Jaar test

---
+++ src/test/java/be/vdab/JaarTest.java
@@ -0,0 +1,33 @@
+package be.vdab;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JaarTest {
+
+    @Test
+    @DisplayName("Een jaar deelbaar door 400 is een schrikkeljaar")
+    public void eenJaarDeelbaarDoor400IsEenSchrikkelJaar() {
+        var jaar = new Jaar(2000);
+        assertThat(jaar.isSchrikkeljaar()).isTrue();
+    }
+
+    @Test
+    void eenJaarDeelbaarDoor100IsGeenSchrikkeljaar() {
+        assertThat(new Jaar(1900).isSchrikkeljaar()).isFalse();
+    }
+    @Test
+    void eenJaarDeelbaarDoor4IsEenSchrikkeljaar() {
+        assertThat(new Jaar(2012).isSchrikkeljaar()).isTrue();
+    }
+    @Test
+    void eenJaarNietDeelbaarDoor4IsGeenSchrikkeljaar() {
+        assertThat(new Jaar(2015).isSchrikkeljaar()).isFalse();
+    }
+
+    @Test
+    void toStringGeefHetJaarAlsTekst() {
+        assertThat(new Jaar(2015)).hasToString("2015");
+    }
+}
