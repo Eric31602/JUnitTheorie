7007913e0c36e9258924d34a8bbffac0da3348d0
Eric Tatarli <eric.ed.tatarlihotmail.com>
2026-01-19 16:04:21 +0100

    Converter test

---
+++ src/test/java/be/vdab/ConverterTest.java
@@ -0,0 +1,20 @@
+package be.vdab;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConverterTest {
+
+    @Test
+    public void eenInchIs2Komma54Centimeter() {
+        assertThat(new Converter().inchesNaarCentimeter(BigDecimal.ONE)).isEqualByComparingTo("2.54");
+    }
+
+    @Test
+    public void tweehonderdInchesIs508Centimeter() {
+        assertThat(new Converter().inchesNaarCentimeter(BigDecimal.valueOf(200))).isEqualByComparingTo("508");
+    }
+}
