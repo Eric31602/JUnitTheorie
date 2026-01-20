e34d49ab3a7be434b47cef7c112b7b8628d2ea6e
Eric Tatarli <eric.ed.tatarlihotmail.com>
2026-01-20 12:04:46 +0100

    Rekening Test met beforeEach

--- src/test/java/be/vdab/RekeningTest.java
+++ src/test/java/be/vdab/RekeningTest.java
@@ -1,5 +1,6 @@
 package be.vdab;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -9,22 +10,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RekeningTest {
 
+    private Rekening rekening;
+
+    @BeforeEach
+    void beforeEach() {
+        rekening = new Rekening();
+    }
+
     @Test
     void hetSaldoVanEenNieuweRekeningIs€0() {
-        var rekening = new Rekening();
         assertThat(rekening.getSaldo()).isZero();
     }
 
     @Test
     void na€10StortingIsSaldo€10() {
-        var rekening = new Rekening();
         rekening.stort(BigDecimal.TEN);
         assertThat(rekening.getSaldo()).isEqualByComparingTo("10");
     }
 
     @Test
     void na€10En€1StortenIsHetSaldo€11() {
-        var rekening = new Rekening();
         rekening.stort(BigDecimal.ONE);
         rekening.stort(BigDecimal.TEN);
         assertThat(rekening.getSaldo()).isEqualByComparingTo("11");
